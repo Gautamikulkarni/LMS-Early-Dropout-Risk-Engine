7f088394d35930d1fb2860c841ed3ea8edcfa6d0
Gautami Kulkarni <gautamik.2005@gmail.com>
2026-03-07 01:19:40 +0530

    Update CORS middleware to use specific origins

--- app.py
+++ app.py
@@ -13,9 +13,14 @@ import json
 
 app = FastAPI()
 
+origins = [
+    "https://s3datasetbucket.s3.ap-south-1.amazonaws.com",
+    "http://localhost:3000",
+    "*"
+]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],   
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
